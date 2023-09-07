2bf3d6635b01bdf77aa080749583de3c3b34ebc4
varashus <rigodavid100@gmail.com>
2023-09-07 09:50:38 +0200

    Add project files.

---
+++ ConsoleApp65/Kategoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp65
+{
+    class Kategoria
+    {
+        public string KategoriaNev { get; set; }
+        public int TulelokSzama { get; set; }
+        public int EltuntekSzama { get; set; }
+
+        public Kategoria(string beolvasottSor)
+        {
+
+            var darabok = beolvasottSor.Split(';');
+            this.KategoriaNev = darabok[0];
+            this.TulelokSzama = int.Parse(darabok[1]);
+            this.EltuntekSzama = int.Parse(darabok[2]);
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp65/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+        }
+
+
+
+    }
+}
